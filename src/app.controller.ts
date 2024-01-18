8ec3cfaf567af1bd2fb5ddf4f61305f31eb4eeff
Nika munjisvili <nikamunja31415@gmail.com>
2024-01-18 11:47:08 +0400

    add commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
-@Controller()
+@Controller('messages')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  private readonly data = [
+    {
+      id: 1,
+      firstName: 'Johny',
+      lastName: 'Cage',
+      age: 27,
+    },
+    {
+      id: 2,
+      firstName: 'Ismoil',
+      lastName: 'Somoni',
+      age: 50,
+    },
+    {
+      id: 3,
+      firstName: 'Luke',
+      lastName: 'Dacascos',
+      age: 12,
+    },
+  ];
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  listMessages() {
+    return this.data;
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
   }
 }
