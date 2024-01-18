3012046ab35b7e2e55f80fdea11d93319fce6496
Nika munjisvili <nikamunja31415@gmail.com>
2024-01-18 15:16:14 +0400

    repository and services

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from '../messages/dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  constructor(public messageService: MessagesService) {}
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messageService.create(body.content);
+  }
+
+  @Get()
+  listMessages() {
+    return this.messageService.findAll();
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const messages = await this.messageService.findOne(id);
+    if (!messages) {
+      const notFoundId = new NotFoundException(
+        `Message with ID "${id}" not found`,
+      );
+      console.log(notFoundId);
+      throw notFoundId;
+    }
+    return messages;
+  }
+}
